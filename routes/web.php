b3e40ac033fcb13c3eba61f8ec61a871adefdf7e
andi9g <andirizky.bayuputra@gmail.com>
2023-07-13 23:03:46 +0700

    oke

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,11 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+
 */
+Route::get('/', function(){
+    return redirect('login');
+});
 Route::get('/logout', 'umumC@logout');
 Route::get('/login', 'umumC@index');
 Route::post('/login', 'umumC@proses')->name('login.proses');
