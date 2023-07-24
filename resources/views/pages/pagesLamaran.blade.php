fbafde90b12d7a0373fbf6875c58e8f0f6bb37cf
andi9g <andirizky.bayuputra@gmail.com>
2023-07-24 22:16:02 +0700

    oke

--- resources/views/pages/pagesLamaran.blade.php
+++ resources/views/pages/pagesLamaran.blade.php
@@ -107,6 +107,7 @@
                                                     <label for='namaberkas' class='text-capitalize'>Pilih Berkas</label>
                                                     <input type='file' name='upload' id='namaberkas' class='form-control' placeholder='masukan namaplaceholder'>
                                                     <p>Format yang di setujui gambar (jpg, jpeg & png) serta dokumen (PDF) </p>
+                                                    <p>Max file yang diupload adalah 2 MB</p>
                                                 </div>
                                             </div>
                                             <div class="modal-footer">
